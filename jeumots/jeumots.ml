f32e507bf3ef1a7966fb3acce7e293ce96a87439
Edouda <edou.aubert@gmail.com>
2022-07-18 19:39:36 +0200

    Jeu mots finalized

--- jeumots/jeumots.ml
+++ jeumots/jeumots.ml
@@ -3,11 +3,17 @@ exception JMException of string;;
 open Printf;;
 
 let () = Random.self_init();;
+
 let usage_msg = " [-nWord] [-play] ... ";;
+
 let input_files = ref [];;
+
 let anon_fun filename = input_files := filename :: !input_files;;
+
 let newWord = ref "";;
+
 let play = ref false;;
+
 let speclist =
   [("-nWord", Arg.Set_string newWord, "Allows to add a new word");
    ("-play",Arg.Set play,"Starts game")];;
@@ -21,6 +27,7 @@ let print_boolean = function
 | false -> print_endline "false";;
 
 let str_to_list str = List.filter (fun h -> if h<>' ' then true else false) (List.init (String.length str) (fun t -> Char.uppercase_ascii  str.[t]));;
+
 let filter_by_char cha lst = List.filter (fun h -> if h<>cha then true else false) lst;;
 
 let rec (>>) value = function
@@ -54,7 +61,7 @@ let wordNo fileName line =
 
 let convertWordToStar word letters = String.map (fun h -> if h >> letters then '*' else h) (word);;
 
-let exitConfirm ansr =if Char.(uppercase_ascii ansr.[0])=='Y' || Char.(uppercase_ascii ansr.[0])=='O' then
+let exitConfirm ansr =if (Char.uppercase_ascii ansr.[0])=='Y' || (Char.uppercase_ascii ansr.[0])=='O' then
                         true
                       else false;;
 
@@ -78,11 +85,12 @@ let printCharList x = print_char '[';
   | [] -> print_char ']';
   | h::t -> print_char h;if t!=[] then print_string " ; ";printCharInside t;
 in printCharInside x ; print_char '\n';;
+
 let rec wordGuess word =
   let rec main guessWord letters userInput=
   try
     (* printCharList letters; *)
-    if (uniDelete userInput.[0] letters)=[] then (
+    if (uniDelete (Char.uppercase_ascii userInput.[0]) letters)=[] then (
       Printf.printf "Et c'est gagné, vous avez trouvé le  mot : %s !\n" guessWord; wordGuess (wordNo "_mots" (Random.int lineCount))
     ) else if (Char.uppercase_ascii userInput.[0]) >> letters then (
       print_endline "Valide, cette lettre est juste !";
@@ -94,19 +102,54 @@ let rec wordGuess word =
     with e -> if exitConfirm (print_endline "Vous avez mal tapé, voulez vous quitter le jeu (y/n) ?"; read_line ()) then exit 0 else main (guessWord) (letters) (Printf.printf "Devinez quel mot se cache derrière %s\n" (convertWordToStar word (uniDelete (Char.uppercase_ascii userInput.[0]) letters)); read_line())
   in main word (str_to_list word |> filter_by_char ' ' |> unify) (Printf.printf "Devinez quel mot se cache derrière %s\n" (convertWordToStar word (str_to_list word |> filter_by_char ' ' |> unify)); read_line ()) ;;
 
-  (* let rec wordPlay *)
+let rec wordPlay word lifeCount =
+  let rec main guessWord letters userInput remainingLifes=
+  try
+    (* printCharList letters; *)
+    (* Printf.printf "%d vie(s) restante(s)" remainingLifes; *)
+    if remainingLifes-1=0 then (
+      Printf.printf "R.I.P.\nTu es mort. Le mot était : %s. Changeons de mot" guessWord; wordPlay (wordNo "_mots" (Random.int lineCount)) 15
+    ) else if (uniDelete (Char.uppercase_ascii userInput.[0]) letters)=[] then (
+      Printf.printf "Et c'est gagné, vous avez trouvé le  mot : %s !\n" guessWord; wordPlay (wordNo "_mots" (Random.int lineCount)) 15
+    ) else if (Char.uppercase_ascii userInput.[0]) >> letters then (
+      print_endline "Valide, cette lettre est juste !";
+      main guessWord (uniDelete (Char.uppercase_ascii userInput.[0]) letters) (Printf.printf "%d vie(s) restante(s)\nRevoici le mot : %s. Choisissez une nouvelle lettre\n" (remainingLifes) (convertWordToStar word (uniDelete (Char.uppercase_ascii userInput.[0]) letters)); read_line()) (remainingLifes)
+    ) else (
+      print_endline "Et non, raté.";
+      main guessWord (uniDelete (Char.uppercase_ascii userInput.[0]) letters) (Printf.printf "%d vie(s) restante(s)\nRevoici le mot : %s. Choisissez une nouvelle lettre\n" (remainingLifes-1) (convertWordToStar word (uniDelete (Char.uppercase_ascii userInput.[0]) letters)); read_line()) (remainingLifes-1)
+    )
+    with e -> if exitConfirm (print_endline "Vous avez mal tapé, voulez vous quitter le jeu (y/n) ?"; read_line ()) then exit 0 else main (guessWord) (letters) (Printf.printf "%d vie(s) restante(s)\nDevinez quel mot se cache derrière %s\n" (remainingLifes) (convertWordToStar word (letters)); read_line()) (remainingLifes)
+  in main word (str_to_list word |> filter_by_char ' ' |> unify) (Printf.printf "%d vie(s) restante(s)\nDevinez quel mot se cache derrière %s\n" (lifeCount) (convertWordToStar word (str_to_list word |> filter_by_char ' ' |> unify)); read_line ()) (lifeCount);;
+
+let wordList fileName =
+  let ic = open_in fileName in (
+    try 
+      let rec lineSeek lineContent =
+        try
+          lineContent :: lineSeek (input_line ic)
+        with e -> [lineContent]
+    in lineSeek (input_line ic);
+  with e ->                      (* some unexpected exception occurs *)
+    close_in_noerr ic;           (* emergency closing *)
+    raise e
+  );;
 
 let main = 
   if !newWord<>"" then (
       (* Write message to file *)
       let oc = open_out_gen [Open_append] (0o777) (file) in    (* create or truncate file, return channel *)
-      fprintf oc "%s\n" (String.uppercase_ascii (!newWord));  (* write something *)   
-      close_out oc;                (* flush and close the channel *)
+      if not (String.uppercase_ascii !newWord >> wordList file) then (
+          fprintf oc "%s\n" (String.uppercase_ascii (!newWord));  (* write something *)   
+          close_out oc;                (* flush and close the channel *)
+        ) else (
+          print_endline "The word is already in the guesslist !";
+          close_out oc;                (* flush and close the channel *)
+        )
   ) 
   else if !play then (
     let ic = open_in file in (
       try (
-        print_endline ((string_of_int lineCount)^" Words."); wordGuess (wordNo "_mots" (Random.int lineCount))
+        print_endline ((string_of_int lineCount)^" Words."); wordPlay (wordNo "_mots" (Random.int lineCount)) 15
       )
       with e -> (                    (* some unexpected exception occurs *)
         close_in_noerr ic;           (* emergency closing *)
