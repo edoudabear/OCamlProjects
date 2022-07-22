b9218ef164502538de61e36cbecfc1cec4f8ee3f
Edouda <edou.aubert@gmail.com>
2022-07-22 09:32:21 +0200

    Updated reqs.ml file

--- reqs/reqs.ml
+++ reqs/reqs.ml
@@ -7,30 +7,30 @@ open Yojson;;
 let get () =
   print_endline "Performing req 1.." ; Quests.get "https://e.diskloud.fr/spotipyAPI"
     ~params:[ ("key1", "value1"); ("key2", "value2") ]
-  >|= Quests.Response.show >|= print_endline;;
+  >|= Quests.Response.content >|= print_endline;;
 
 let post_form () =
-  print_endline "Performing req 2.." ; Quests.post "https://e.diskloud.fr/Dilab/get" ~data:(Form [ ("type", "mainGroups") ])
+  print_endline "\nPerforming req 2.." ; Quests.post "https://e.diskloud.fr/Dilab/get" ~data:(Form [ ("type", "mainGroups") ])
   >|= Quests.Response.show >|= print_endline;;
 
 let post_json () =
-  Quests.post "https://e.diskloud.fr/Dilab/get" ~data:(Json [%yojson { content = "test" }])
+  print_endline "\nPerforming req 3.." ; Quests.post "https://e.diskloud.fr/Dilab/get" ~data:(Json [%yojson { content = "test" }])
   >|= Quests.Response.show >|= print_endline;;
 
 let gzip_response () =
-  print_endline "Performing req 3.." ; Quests.get "https://e.diskloud.fr/spotipyAPI"
+  print_endline "\nPerforming req 4.." ; Quests.get "https://e.diskloud.fr/spotipyAPI"
   >|= Quests.Response.show >|= print_endline;;
 
 let following_redirects () =
-  print_endline "Performing req 4.." ; Quests.get "https://e.diskloud.fr/spotipyAPI"
+  print_endline "\nPerforming req 5.." ; Quests.get "https://e.diskloud.fr/spotipyAPI"
   >|= Quests.Response.show >|= print_endline;;
 
 let basic_authentication () =
-  print_endline "Performing req 5.." ; Quests.get "https://e.diskloud.fr/spotipyAPI" ~auth:(Basic ("Edouda", "test"))
+  print_endline "\nPerforming req 6.." ; Quests.get "https://e.diskloud.fr/spotipyAPI" ~auth:(Basic ("Edouda", "test"))
   >|= Quests.Response.show >|= print_endline;;
 
 let bearer_authentication () =
-  print_endline "Performing req 6.." ; Quests.get "https://example.com/some-api" ~auth:(Bearer "a token")
+  print_endline "\nPerforming req 7.." ; Quests.get "https://example.com/some-api" ~auth:(Bearer "a token")
   >|= Quests.Response.show >|= print_endline;;
 
 let sessions () =
@@ -39,7 +39,7 @@ let sessions () =
   let%lwt () =
     Session.get s "https://example.com" >|= Response.show >|= print_endline
   in
-  Session.close s;;
+  print_endline "\nRequests finished" ; Session.close s;;
 
 let () =
   Lwt_main.run
