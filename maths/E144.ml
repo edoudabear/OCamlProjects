eb37f64ffb5007b2614a2479508d930ef39f207f
Edouda <edou.aubert@gmail.com>
2022-08-02 18:40:39 +0200

    Added comments inside E144.ml file inside maths dir

--- maths/E144.ml
+++ maths/E144.ml
@@ -19,8 +19,10 @@ in aux [] lst;;
 
 let u n = (range n) |> List.map (fun h -> n/h) |> delCopies |> List.length;;
 
+(* Indiviual index (n) *)
 (* let _ = print_int(u (print_endline "Enter series index :"; read_int ()) ); print_string "\n\n";; *)
 
+(* Range version (from 1 to n) *)
 let _ =
   let rec aux i n= if i>n then exit 0 else Printf.printf "%d\t\t%d\n" (i) (u i); aux (i+1) n 
 in aux (1) (print_endline "Enter series index range (1-'your answer') :" ; read_int ())
